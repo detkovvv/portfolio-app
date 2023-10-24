217a95fcd02e866a4704db9b846cf24e09f218e5
Detkov Vasiliy <fgh.detkov@gmail.com>
2023-10-24 17:04:51 +0300

    refactoring Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,6 +7,9 @@ const Navbar = () => {
 
     const activeLink = 'nav-list__link nav-list__link--active';
     const normalLink = 'nav-list__link';
+    const classDefinition = ({isActive}) => {
+        return isActive ? activeLink : normalLink;
+    }
 
     return (
         <div>
@@ -19,16 +22,13 @@ const Navbar = () => {
                         <BtnDarkMode/>
                         <ul className="nav-list">
                             <li className="nav-list__item">
-                                <NavLink to="/"
-                                         className={({isActive}) => isActive ? activeLink : normalLink}>Home</NavLink>
+                                <NavLink to="/" className={classDefinition}>Home</NavLink>
                             </li>
                             <li className="nav-list__item">
-                                <NavLink to="/projects"
-                                         className={({isActive}) => isActive ? activeLink : normalLink}>Projects</NavLink>
+                                <NavLink to="/projects" className={classDefinition}>Projects</NavLink>
                             </li>
                             <li className="nav-list__item">
-                                <NavLink to="/contacts"
-                                         className={({isActive}) => isActive ? activeLink : normalLink}>Contacts</NavLink>
+                                <NavLink to="/contacts" className={classDefinition}>Contacts</NavLink>
                             </li>
                         </ul>
                     </div>
