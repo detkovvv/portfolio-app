21aaeb444cbe192451432f153b519f346dcd51f2
Detkov Vasiliy <fgh.detkov@gmail.com>
2023-10-25 15:41:24 +0300

    add vite-plugin-checker

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "./",
+  plugins: [react(), checker(
+    {
+      // e.g. use TypeScript check
+      typescript: true,
+    }
+  )],
+  base: './',
 })
